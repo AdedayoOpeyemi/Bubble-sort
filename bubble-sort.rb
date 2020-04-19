ab431d8737c774f30d58b3741acf8ab445c4c7cb
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2020-04-19 20:42:21 +0100

    Added bubble_sort_by method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -15,3 +15,28 @@ for i in 0...arr.length-n
 end
 arr
 end
+
+def bubble_sort_by(arr)
+
+    arr_len = arr.length
+    swap_occur = true
+    i = 1
+
+    while swap_occur
+      swap_occur = false
+
+      (0...arr_len - i ).each do |x|
+        next unless yield(arr[x], arr[x+1]) >= 1
+          arr[x], arr[x + 1] = arr[x + 1], arr[x]
+          swap_occur = true
+        end
+        i += 1
+      end
+      arr
+end
+
+
+
+
+print bubble_sort_by(["hello", "hey", "hi"]) { |left, right| left.length - right.length } 
+  
