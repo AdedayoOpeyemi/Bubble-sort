784224d0e5998d25c562ef97a8b6b561e93a3f32
moma <angelmomaa@gmail.com>
2020-04-19 16:32:47 -0500

    linters

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,14 @@
 def bubble_sort(arr)
   n = 1
-  (0..arr.length-n).each do |i|
-    j = 0
-    (0...arr.length-1).each do |j|
-      if arr[j] > arr[j+1]
-        arr[j], arr[j+1] = arr[j+1], arr[j]
-      end    
+  (0..arr.length - n).each do |i|
+  j = 0
+    (0...arr.length - 1).each do |j|
+      arr[j], arr[j+1] = arr[j+1], arr[j] if arr[j] > arr[j+1]    
     end
-    i+=1
-    n+=1
-end
+    n += 1
+  end
   arr
 end
-
-print bubble_sort([1,7,2,9,3,4])
 
 def bubble_sort_by(arr)
   arr_len = arr.length
